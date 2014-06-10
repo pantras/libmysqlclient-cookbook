4cd9b454a8e925efbb131475737be02a67062f58
Mike Splain <msplain@paypal.com>
2014-06-10 18:15:35 -0400

    Update license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'libmysqlclient'
 maintainer       'Mike Splain'
 maintainer_email 'mike.splain@gmail.com'
-license          ''
+license          'Apache v2.0'
 description      'Installs/Configures libmysqlclient-cookbook'
 long_description 'Installs/Configures libmysqlclient-cookbook'
 version          '0.1.0'
